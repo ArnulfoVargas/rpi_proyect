9c36d025b80e3afe5a095f420ed3eb5b8e920eb5
ArnulfoVargas <vargas.mejia.arnulfo04@gmail.com>
2023-11-24 07:27:38 -0600

    bugged

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
 mod  routes;
 use actix_web::{HttpServer, App, web};
+use std::thread;
+use std::time::Duration;
 
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main(){
 
+    let t1 = thread::spawn(|| {
+        println!("Running");
+        run_app();
+
+        println!("Running");
+        thread::sleep(Duration::from_secs(100));
+    });
+
+    
+    t1.join().unwrap();
+    ()
+}
+
+
+async fn run_app() -> std::io::Result<()>{
+    println!("Running");
     HttpServer::new(|| {
         App::new()
         .route("/", web::get().to(routes::get_index))
         .service(routes::receive_data)
     })
-    .bind("127.0.0.1:3000")?
-    .run()
-    .await
+    .bind("127.0.0.1:3000")?.run().await
 }
